90cae44184906a9656a39e94ee2e6ccfa7ee66b3
Guilherme <you@example.com>
2025-06-22 14:22:42 -0300

    Corrigido arquivo configs.js

--- src/configs.js
+++ src/configs.js
@@ -1,4 +1,4 @@
-export default {
+const config = {
     apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
     databaseURL: process.env.REACT_APP_DATABASEURL,
@@ -8,4 +8,5 @@ export default {
     appId: process.env.REACT_APP_APPID,
     measurementId: process.env.REACT_APP_MEASUREMENTID,
     api_url: process.env.REACT_APP_APIURL,
-}
+}  
+export default config;
